1e5c69cfa3cd2085817e1c1c9f417010800b209f
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2021-12-02 12:58:11 +0500

    Documentation in process

---
+++ login.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Admin" && password === "Evtl123") {
+    	localStorage.setItem("Admin", "Evtl123");
+        alert("You have successfully logged in.");
+        location.assign("index.html");
+        // location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
